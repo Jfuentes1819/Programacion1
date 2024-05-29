35327ed6071fb355f07a4537ddacd1d7f09d8272
Jfuentes1819 <jfuentes0619@gmail.com>
2024-05-29 08:59:03 -0400

    Initial commit

---
+++ Practica.cs
@@ -0,0 +1,69 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+Console.Write("Hola");
+
+Console.ReadKey();
+
+decimal salario;
+double exponente;
+string apellido;
+char sexo;
+bool activo;
+bool inactivo;
+
+// Nombre de una persona
+
+string nombre;
+
+// Edad de una persona
+
+int edad;
+// Altura de una persona en metros (con decimales)
+
+decimal alturaMetros;
+
+// Número de matrícula de un estudiante (sin decimales)
+
+int matricula;
+
+// Promedio de calificaciones (con decimales)
+
+float promedio;
+
+// Estado civil(soltero, casado, etc.)
+
+string estadoCivil;
+
+// Género(M / F)
+
+char genero;
+
+// Estado de inscripción (verdadero o falso)
+
+bool inscripcion;
+
+// Temperatura en grados Celsius
+
+int temperaturaCelcius;
+
+// Número de hijos
+
+int hijos;
+
+// Fecha de nacimiento
+
+int nacimiento;
+
+// Costo de un producto en una tienda
+
+float costo;
+
+// Respuesta a una pregunta (sí/no)
+
+    char respuesta;
+
+// Código postal 
+
+    int codigoPostal;
+
